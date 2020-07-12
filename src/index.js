727ea63d1e0a4f097df9257f0507c67adb07b58b
Pablo Hildo <pablohildo@gmail.com>
2020-07-12 00:34:28 -0300

    Actually removes service worker

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import './main.css';
 import { Elm } from './Main.elm';
-import * as serviceWorker from './serviceWorker';
 
 const enableDarkMode = () => {
   const root = document.documentElement;
@@ -37,8 +36,3 @@ app.ports.toggleDarkMode.subscribe((isDarkModeOn) => {
     disableDarkMode();
   }
 });
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
